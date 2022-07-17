fd5425a38eb1a761239ea4c502284598e15568ad
Rama Krishnan <96856179+marvelxcodes@users.noreply.github.com>
2022-07-17 16:11:20 +0400

    Delete LocateCard.py

--- Algorithms/LocateCard.py
+++
@@ -1,7 +0,0 @@
-def LocateCard(cards: list[int], query:int):
-    position = -1
-    for i in range(0, len(cards)):
-        if cards[i] == query:
-            position = i
-            break
-    return position
